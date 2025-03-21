61293c46557e48adc5aaad12952736a8d262ba60
David <david.leon.m@gmail.com>
2025-03-21 12:56:07 +0100

    provider

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-provider "kubernetes" {}
-
-terraform {
-  required_version = ">= 1.10.0"
-
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.34.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.6.3"
-    }
-
-    tls = {
-      source  = "hashicorp/tls"
-      version = "4.0.6"
-    }
-  }
-
-  backend "kubernetes" {}
-}
-
 resource "kubernetes_namespace" "namespace" {
   metadata {
     name = var.namespace
---
+++ provider.tf
@@ -0,0 +1,24 @@
+provider "kubernetes" {}
+
+terraform {
+  required_version = ">= 1.10.0"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.34.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.6"
+    }
+  }
+
+  backend "kubernetes" {}
+}
